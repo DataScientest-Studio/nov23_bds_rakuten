0e416b37c0acd72f96ab98d3368b22e92b9c6dc0
Karim HADJAR <karim1979fr@gmail.com>
2024-02-16 15:04:04 +0100

    modify prepro text

--- streamlit/tabs/preprocessing.py
+++ streamlit/tabs/preprocessing.py
@@ -11,26 +11,54 @@ def renderPreprocessing():
 
     if selected3=="Texte":
         st.header('Préparation du texte')
-        row1 = st.columns(2)
-        row2 = st.columns(2)
+        
+        with st.expander('Techniques de préparation du texte'):
+            #st.header('Préparation du texte')
+            st.markdown("""
+                **Préparation du texte**  
+                Application de techniques statistiques et d'apprentissage machine traditionnel pour la classification de texte, avec une préparation spécifique du texte.
 
-        texte = ["""
-        **Texte non traduit avec Filtrage des stop words, WordNetLemmatizer, et TF-IDF :** 
-        Cette approche a permis de réduire le bruit dans les données textuelles et de concentrer l'analyse sur les termes significatifs.""",
+                **1. Nettoyage du Texte:**  
+                Suppression des balises Html, de la ponctuation.
 
-        """**Texte traduit avec CBOW (Continuous Bag of Words) :** 
-        Les textes ont été vectorisés en utilisant l'approche CBOW de Word2Vec, permettant de capturer le contexte des mots dans les vecteurs.""",
+                **2. Tokenisation:**  
+                Division du texte en mots ou phrases (tokens) pour faciliter leur traitement.
 
-        """**Skip Gram :** 
-        Une autre technique de Word2Vec, Skip Gram, a été utilisée pour prédire le contexte à partir des mots, offrant une alternative à CBOW.""",
+                **3. Suppression des mots vides (stop words):**  
+                Les mots très fréquents (comme "le", "et", "à") qui n'apportent pas de valeur significative pour l'analyse sont retirés.
 
-        """**Texte traduit tokenisé avec padding :** 
-        Cette méthode a préparé les données pour les modèles de réseaux de neurones récurrents (RNN) en normalisant la longueur des séquences de texte.
-        """]
+                **4. Normalisation:**  
+                Application de la lemmatisation ou du stemming pour réduire les mots à une forme de base.
 
-        for i, col in enumerate(row1 + row2):
-            tile = col.container(height=120)
-            tile.markdown(texte[i])
+                **5. Vectorisation et padding:**  
+                Transformation du texte en vecteurs numériques que le modèle peut traiter. Le sens des phrases peut être perdu dans cette étape.                                                        
+                Ceci peut être réalisé par des techniques comme:
+                - Word2Vec : CBOW (Continuous Bag of Words) ou Skip Gram
+                - TF-IDF
+                - Des embeddings contextuels (BERT, GPT)
+                """, unsafe_allow_html=True)
+            
+        with st.expander('Techniques principales appliquées'):        
+            row1 = st.columns(2)
+            row2 = st.columns(2)
+
+            texte = ["""
+                **Texte non traduit avec Filtrage des stop words, WordNetLemmatizer, et TF-IDF :**         
+                Cette approche a permis de réduire le bruit dans les données textuelles et de concentrer l'analyse sur les termes significatifs.""",
+
+                """**Texte traduit avec CBOW (Continuous Bag of Words) :**                                   
+                Les textes ont été vectorisés en utilisant l'approche CBOW de Word2Vec, permettant de capturer le contexte des mots dans les vecteurs.""",
+
+                """**Skip Gram :**                                                                             
+                Une autre technique de Word2Vec, Skip Gram, a été utilisée pour prédire le contexte à partir des mots, offrant une alternative à CBOW.""",
+
+                """**Texte traduit tokenisé avec padding :**                                                       
+                Cette méthode a préparé les données pour les modèles de réseaux de neurones récurrents (RNN) en normalisant la longueur des séquences de texte.
+                """]
+
+            for i, col in enumerate(row1 + row2):
+                tile = col.container(height=120)
+                tile.markdown(texte[i])
 
     if selected3=='Images':
         st.header('Preprocessing des images')
